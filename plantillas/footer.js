9f7ac6f497ce6c8d713e54d76148698f15a5b0cf
Gualterosky <87479577+Gualterosky@users.noreply.github.com>
2024-07-29 11:13:00 -0500

    responsive and login1

---
+++ plantillas/footer.js
@@ -0,0 +1,12 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var navContainer = document.createElement('nav');
+    navContainer.innerHTML = `
+        <aqui va el codigo html>
+<style>
+
+</style>
+    `;
+    document.body.appendChild(navContainer);
+});
