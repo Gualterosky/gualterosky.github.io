03358009f1462ce93a72929949ed2b241a8810e7
Gualterosky <87479577+Gualterosky@users.noreply.github.com>
2024-08-04 19:58:06 -0500

    biodiversidad en choachi

---
+++ Biodiversidad_choachi/plantilla/nav.js
@@ -0,0 +1,59 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var navContainer = document.createElement('nav');
+    navContainer.innerHTML = `
+<nav>
+        <ul>
+            <li><a href="#"><img src="image.png" alt="Icono" width="180px"></a></li>
+            <li><a href="#s">Animales</a></li>
+            <li><a href="#s">Plantas</a></li>
+            <li><a href="/">Ecosistemas</a></li>
+            <li><a href="/">Mapa</a></li>
+        </ul>
+</nav>
+<style>
+nav{/*Menu*/
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 60px;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.2);
+    backdrop-filter: blur(10px); /* Efecto de desenfoque */
+}
+
+nav a{
+    color: hsl(0, 0%, 100%);
+    font-size: large;
+    display: block;
+    text-decoration: none;
+    padding: 0 2.5em;
+    transition: .4s;
+}
+
+nav a:hover{ /*casilla al pasar el raton*/
+    background: rgba(0, 0, 0, 0.5);
+}
+
+nav li{
+    line-height: 3rem;
+}
+
+nav ul{
+    list-style: none;
+    padding: 0%;
+}
+nav > ul{
+    background-color: none;
+    display: table;
+    width: 100%;
+}
+
+nav > ul > li{
+    float: left;
+}
+</style>
+    `;
+    document.body.appendChild(navContainer);
+});
